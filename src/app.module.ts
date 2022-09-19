b70d465a379e72583205b15349c49ab9666d856d
amirmohammadBeheshti <bamir9707@gmail.com>
2022-09-19 11:35:52 +0430

    sds

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    AuthModule,
+    UsersModule,
     ConfigModule.forRoot({}),
     MongooseModule.forRoot('mongodb://localhost:27017/store'),
   ],
--- src/auth/auth.serialize.ts
+++
@@ -1,9 +0,0 @@
-import { CreateUserDto } from './dto/createUser.dto';
-import { Auth } from './schema/auth.schema';
-
-export class serializeUser {
-  email: string;
-  constructor(entity: Partial<CreateUserDto>) {
-    this.email = entity.email;
-  }
-}
